501dc2b9b60842d06cb1958ad0e30767cdcc4dd4
jameson141 <jamesonmiyashiro@gmail.com>
2017-09-22 10:20:20 -1000

    modified:   client.js 	modified:   index.html

--- client.js
+++ client.js
@@ -1,15 +1,22 @@
-let WHYYYYYYYYYYY = [];
+function buildHTMLElements (responseData) {
+	let title = $("<h2 class='film-title'></h2>").text("Title");
+	let director = $("<h2 class='film-director'></h2>").text("Director");
+	$(".films").append(title, director);
+};
+
+
 
 $("button").click(function(){
 	$.ajax({
 		url:"https://swapi.co/api/films/",
 		type: "GET",
 		success: function getFilmData (data){
-				WHYYYYYYYYYYY.push(JSON.parse);
-				console.log(WHYYYYYYYYYYY);
+			// console.log(data.results[0]);
+			$(".film-title").append(data.results[0].title);
 		},
 	});
 });
+
 
 
 // console.log("die")
